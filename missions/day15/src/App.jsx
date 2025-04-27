fdfeefdbd7820d3645b2e433d881685f31d024e9
sloth-bear <slothbear.hj@gmail.com>
2025-04-27 17:05:56 +0900

    Fix a bug

--- missions/day15/src/App.jsx
+++ missions/day15/src/App.jsx
@@ -18,13 +18,16 @@ function reducer (state, action) {
 }
 
 function App() {
+  const [lastId, setLastId] = useState(3);
+
   const [contacts, dispatch] = useReducer(reducer, [
     { id: 1, name: "한입스튜디오", email: "onebite.fe@gmail.com" },
     { id: 2, name: "이정환", email: "king199777@gmail.com" },
   ]);
 
   const onSubmit = (data) => {
-    dispatch({ type: "ADD", data });
+    dispatch({ type: "ADD", data: { ...data, id: lastId }});
+    setLastId(lastId + 1);
   }
 
   const onDelete = (id) => {
